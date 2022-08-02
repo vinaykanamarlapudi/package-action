1e2acc1c021356ee12f93129aae2ac104fe8d4ff
Ruchika Jain <31808822+ruchika-jain@users.noreply.github.com>
2022-08-02 15:46:28 +0000

    Add publish function

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
+import axios from 'axios'
 import {wait} from './wait'
 
 async function run(): Promise<void> {
@@ -13,24 +14,48 @@ async function run(): Promise<void> {
 
     core.setOutput('time', new Date().toTimeString())
 
-    const TOKEN: string = core.getInput('token');
-    core.setSecret(TOKEN);
-    const repoInput: string = core.getInput('repository');
+    
+    const repoInput: string = process.env.GITHUB_REPOSITORY;
     const repoDetails: string[] = repoInput.split("/");
     const repositoryOwner: string = repoDetails[0];
     const repositoryName: string = repoDetails[1];
     const semver: string = core.getInput('semver');
 
-    await publishOciArtifact(repoDetails, semver);
+    await publishOciArtifact(repoInput, semver);
 
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
 
-async function publishOciArtifact(repoDetails: string[], semver: string): Promise<void> {
+async function createTarBall(workdir: string): Promise<void> {
   try {
-    
+    console.log("Insert logic to create Tarball");
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(`Oops! Creation of tarball failed!`)
+  }
+}
+
+async function publishOciArtifact(repoInput: string, semver: string): Promise<void> {
+  try {
+    const TOKEN: string = core.getInput('token');
+    core.setSecret(TOKEN);
+    const workdir: string = core.getInput('workdir');
+    const publishPackageEndpoint: string = `http://localhost:3000/repos/${repoInput}/actions/package/${semver}`;
+    const payload = {
+      tarball: await createTarBall(workdir)
+    }
+    core.info(`Creating GHCR package with payload:\n${JSON.stringify(payload, null, '\t')}`)
+    const response = await axios.post(publishPackageEndpoint, payload, {
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+        Authorization: `Bearer ${TOKEN}`,
+        'Content-type': 'application/json'
+      }
+    });
+    if (response && response.data) {
+      core.info(JSON.stringify(response.data))
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(`Oops! Action package push to GHCR failed!`)
   }
