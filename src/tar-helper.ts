a79de811740ed265626ec62528b34885dd94cb61
Ruchika Jain <31808822+ruchika-jain@users.noreply.github.com>
2022-08-12 19:44:03 +0530

    Create tar-helper.ts

---
+++ src/tar-helper.ts
@@ -0,0 +1,23 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import tar from 'tar';
+
+export async function createTarBall(workdir: string): Promise<void> {
+  try {
+    const tarStream = tar.c(
+      {
+        gzip: true
+      },
+      ['./']
+   );
+    console.log(`Tar.create added`);
+    console.log(tarStream);
+    await exec.exec(`touch archive.tar.gz`);
+    await exec.exec(`tar --exclude=archive.tar.gz -czf archive.tar.gz ${workdir}`);
+    console.log("Tar cmd done");
+    console.log("Exec executing for ls down");
+    exec.exec('ls');
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(`Oops! Creation of tarball failed!`)
+  }
+}
