877fb8717d1eac8f69082b245a13db265433fb5d
Ruchika Jain <31808822+ruchika-jain@users.noreply.github.com>
2022-08-23 17:25:17 +0530

    Update tar-helper.ts

--- src/tar-helper.ts
+++ src/tar-helper.ts
@@ -10,15 +10,8 @@ export function createTarBall(workdir: string): void {
       },
       ['./']
    );
-    console.log(`Tar.create added`);
-    console.log(tarStream);
     exec.exec(`touch archive.tar.gz`);
     exec.exec(`tar --exclude=archive.tar.gz -czf archive.tar.gz ${workdir}`);
-    console.log("Tar cmd done");
-    console.log("Exec executing for ls down");
-    exec.exec('ls');
-    exec.exec('tar -tf archive.tar.gz');
-    core.info(`Just before this contents into archive.tar.gz`);
     core.info(`Tar ball created.`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(`Oops! Creation of tarball failed!`)
