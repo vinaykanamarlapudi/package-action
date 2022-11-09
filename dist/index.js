ac0a78c58314104afd6894f133cb4694e1e952bd
Vinay Kanamarlapudi <100673363+vinaykanamarlapudi@users.noreply.github.com>
2022-11-09 21:35:33 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -173,8 +173,8 @@ function run() {
 //             }
             const path = core.getInput('path');
             const tarBallCreated = yield tarHelper.createTarBall(path);
-            const releaseId = github.event.client_payload.release.id;
-            const semver = github.event.client_payload.release.tag_name;
+            const releaseId = github.context.payload.client_payload.release.id;
+            const semver = github.context.payload.client_payload.release.tag_name;
             if (tarBallCreated) {
                 yield apiClient.publishOciArtifact(repository, releaseId, semver);
             }
