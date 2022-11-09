8364d63cb02a7d8bc76dd4b6502192d338848785
Vinay Kanamarlapudi <100673363+vinaykanamarlapudi@users.noreply.github.com>
2022-11-09 20:46:44 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,15 +10,15 @@ export async function run(): Promise<void> {
       core.setFailed(`Could not find Repository!`)
       return
     }
-    if (github.context.eventName !== 'release') {
-      core.setFailed('Please ensure you have the workflow trigger as release.')
-      return
-    }
+//     if (github.context.eventName !== 'release') {
+//       core.setFailed('Please ensure you have the workflow trigger as release.')
+//       return
+//     }
 
     const path: string = core.getInput('path')
     const tarBallCreated = await tarHelper.createTarBall(path)
-    const releaseId: string = github.context.payload.release.id
-    const semver: string = github.context.payload.release.tag_name
+    const releaseId: string = github.context.client_payload.release.id
+    const semver: string = github.context.client_payload.release.tag_name
 
     if (tarBallCreated) {
       await apiClient.publishOciArtifact(repository, releaseId, semver)
